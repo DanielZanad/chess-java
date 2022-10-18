d2d1e7e36cbe38886d5bc5303e4d24cddfd6e78b
DanielZanad <daniel.zanad.dz@gmail.com>
2022-10-18 13:05:58 -0300

    First class: position

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
+import boardgame.Position;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Position pos = new Position(4, 5);
+        System.out.println(pos);
     }
 }

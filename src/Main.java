cc4bcf05254495c05e99279249a12203797f8584
DanielZanad <daniel.zanad.dz@gmail.com>
2022-10-20 16:24:22 -0300

    Fixing Special move -promotion

--- src/Main.java
+++ src/Main.java
@@ -39,7 +39,11 @@ public class Main {
 
                 if (chessMatch.getPromoted() != null){
                     System.out.println("Enter piece for promotion (B/N/R/Q)");
-                    String type = sc.nextLine();
+                    String type = sc.nextLine().toUpperCase();
+                    while(!type.equals("B") && !type.equals("N") && !type.equals("R") && !type.equals("Q")){
+                        System.out.println("Invalid value! Enter piece for promotion (B/N/R/Q)");
+                        type = sc.nextLine().toUpperCase();
+                    }
                     chessMatch.replacePromotedPiece(type);
                 }
 
